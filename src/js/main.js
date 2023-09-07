96e5d6d8d0a2ec7d1b7b35a64e61ffc7ffc87f87
OnlySig <gabriel.castro.pereira11@gmail.com>
2023-09-06 22:19:01 -0300

    add: ligthBoxIcons + responsividade rework: js/imgSecImages

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,36 @@ function lightBox() {
     const corpo = document.body
     const elementIcon = document.createElement("i");
     const imgPrincipal = document.createElement("img");
+    const imgSecContainer = document.createElement("div");
+
+    const imgSecItem1 = document.createElement("img");
+    const imgSecItem2 = document.createElement("img");
+    const imgSecItem3 = document.createElement("img");
+    const imgSecItem4 = document.createElement("img");
     
+    const iconeSlider1 = document.createElement("i");
+    const iconeSlider2 = document.createElement("i");
+
+    const imgSlider__container = document.createElement("div")
+
+    imgSecItem1.src = "../../images/image-product-1-thumbnail.jpg"
+    imgSecItem2.src = "../../images/image-product-2-thumbnail.jpg"
+    imgSecItem3.src = "../../images/image-product-3-thumbnail.jpg"
+    imgSecItem4.src = "../../images/image-product-4-thumbnail.jpg"
+
+    imgSlider__container.classList.add("imgSlider__container");
+
+    iconeSlider1.classList.add("fa-circle-chevron-left");
+    iconeSlider1.classList.add("fa-solid");
+    iconeSlider1.classList.add("p-ab");
+    iconeSlider1.classList.add("pr-l");
+    
+    iconeSlider2.classList.add("fa-circle-chevron-right");
+    iconeSlider2.classList.add("fa-solid");
+    iconeSlider2.classList.add("p-ab");
+    iconeSlider2.classList.add("pr-r");
+
+    imgSecContainer.classList.add("imgSecContainer");
     imgPrincipal.classList.add("principalImg");
     imgPrincipal.src = "../../images/image-product-1.jpg"
     divContent.classList.add("light__content");
@@ -34,11 +63,21 @@ function lightBox() {
     corpo.append(divMain);
     divMain.appendChild(divContent);
     divContent.appendChild(elementIcon);
-    divContent.appendChild(imgPrincipal);
+    imgSlider__container.appendChild(iconeSlider1);
+    imgSlider__container.appendChild(imgPrincipal);
+    imgSlider__container.appendChild(iconeSlider2);
+    divContent.appendChild(imgSlider__container);
+    divContent.appendChild(imgSecContainer);
 
+    imgSecContainer.appendChild(imgSecItem1);
+    imgSecContainer.appendChild(imgSecItem2);
+    imgSecContainer.appendChild(imgSecItem3);
+    imgSecContainer.appendChild(imgSecItem4);
+
+    console.log(corpo)
     
     const tiraBox = document.querySelector(".icone");
     tiraBox.addEventListener("click", _ => {
-        divMain.remove()
+        divMain.remove();
     });
 }
